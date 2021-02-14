b33753885cb2b314238994c67504b83e6df28a7e
Lucas Matos <lucaspmatos@outlook.com>
2021-02-14 03:29:52 -0300

    Main Files and Styling Components

---
+++ frontend/src/components/partials/Header/styled.js
@@ -0,0 +1,35 @@
+import styled from "styled-components";
+
+export const HeaderArea = styled.div`
+  background-color: white;
+  height: 60px;
+  border-bottom: 1px solid #CCC;
+
+  .container {
+    max-width: 1000px;
+    margin: auto;
+    display: flex;
+  }
+
+  a {
+    text-decoration: none;
+  }
+
+  .logo {
+    flex: 1;
+    display: flex;
+    align-items: center;
+    height: 60px;
+
+    .logo-1,
+    .logo-2,
+    .logo-3 {
+      font-size: 27px;
+      font-weight: bold;
+    }
+
+    .logo-1 { color: #ff007f; }
+    .logo-2 { color: lightgreen; }
+    .logo-3 { color: purple; }
+  }
+`;
---
+++ frontend/src/reducers/userReducer.js
@@ -0,0 +1,12 @@
+/* eslint-disable import/no-anonymous-default-export */
+const initialState = {
+  email: "",
+};
+
+export default (state = initialState, action) => {
+  if (action.type === "SET_EMAIL") {
+    return { ...state, email: action.payload.email };
+  }
+
+  return state;
+};
