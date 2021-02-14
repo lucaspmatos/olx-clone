60372a33382d3ab293bec736dc21e202db381755
Lucas Matos <lucaspmatos@outlook.com>
2021-02-14 04:19:39 -0300

    Adding JS Cookie library and coding Authentication Handler

--- frontend/src/components/partials/Header/index.js
+++ frontend/src/components/partials/Header/index.js
@@ -2,7 +2,11 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { HeaderArea } from "./styled";
 
+import { isLogged } from "../../../helpers/AuthHandler";
+
 export default function Header() {
+  let logged = isLogged();
+
   return (
     <HeaderArea>
       <div className="container">
@@ -13,6 +17,40 @@ export default function Header() {
             <span className="logo-3">X</span>
           </Link>
         </div>
+        <nav>
+          <ul>
+            {logged && (
+              <>
+                <li>
+                  <Link to="/my-account">Minha Conta</Link>
+                </li>
+                <li>
+                  <Link to="/logout">Sair</Link>
+                </li>
+                <li>
+                  <Link to="/post-ad" className="button">
+                    Anunciar
+                  </Link>
+                </li>
+              </>
+            )}
+            {!logged && (
+              <>
+                <li>
+                  <Link to="/sign-in">Login</Link>
+                </li>
+                <li>
+                  <Link to="/sign-up">Cadastrar-se</Link>
+                </li>
+                <li>
+                  <Link to="/sign-in" className="button">
+                    Anunciar
+                  </Link>
+                </li>
+              </>
+            )}
+          </ul>
+        </nav>
       </div>
     </HeaderArea>
   );
--- frontend/src/components/partials/Header/styled.js
+++ frontend/src/components/partials/Header/styled.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const HeaderArea = styled.div`
   background-color: white;
   height: 60px;
-  border-bottom: 1px solid #CCC;
+  border-bottom: 1px solid #ccc;
 
   .container {
     max-width: 1000px;
@@ -20,16 +20,67 @@ export const HeaderArea = styled.div`
     display: flex;
     align-items: center;
     height: 60px;
+    margin-left: 10px;
 
     .logo-1,
     .logo-2,
     .logo-3 {
       font-size: 27px;
-      font-weight: bold;
+      font-weight: 900;
     }
 
-    .logo-1 { color: #ff007f; }
-    .logo-2 { color: lightgreen; }
-    .logo-3 { color: purple; }
+    .logo-1 {
+      color: #ff007f;
+    }
+    .logo-2 {
+      color: lightgreen;
+    }
+    .logo-3 {
+      color: purple;
+    }
+  }
+
+  nav {
+    padding-top: 10px;
+    padding-bottom: 10px;
+
+    ul,
+    li {
+      margin: 0;
+      padding: 0;
+      list-style: none;
+    }
+
+    ul {
+      display: flex;
+      align-items: center;
+      height: 40px;
+    }
+
+    li {
+      margin-left: 20px;
+      margin-right: 20px;
+
+      a {
+        color: black;
+        font-size: 14px;
+        text-decoration: none;
+
+        &:hover {
+          color: #999;
+        }
+
+        &.button {
+          background-color: #FF8100;
+          border-radius: 25px;
+          color: white;
+          padding: 5px 10px;
+        }
+
+        &.button:hover {
+          background-color: #E57706;
+        }
+      }
+    }
   }
 `;
