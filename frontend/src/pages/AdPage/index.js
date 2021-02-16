2c1656b59ae27088ad8d636dbcba6c7c9f93136c
Lucas Matos <lucaspmatos@outlook.com>
2021-02-16 03:46:54 -0300

    Setting BreadCrumb on Ad Page

--- frontend/src/pages/AdPage/index.js
+++ frontend/src/pages/AdPage/index.js
@@ -1,7 +1,10 @@
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
-import { PageArea, Fake } from "./styled";
+import { Link, useParams } from "react-router-dom";
+import { Slide } from "react-slideshow-image";
+import "react-slideshow-image/dist/styles.css";
+import { PageArea, Fake, OthersArea, BreadCrumb } from "./styled";
 import { PageContainer } from "../../components/MainComponents";
+import AdItem from "../../components/partials/AdItem";
 
 import useApi from "../../helpers/OlxAPI";
 
@@ -49,10 +52,37 @@ export default function Page() {
 
   return (
     <PageContainer>
+      {adInfo.category && (
+        <BreadCrumb>
+          Você está aqui:
+          <Link to="/">Home</Link>
+          &gt;
+          <Link to={`/ads?state=${adInfo.stateName}`}>{adInfo.stateName}</Link>
+          &gt;
+          <Link
+            to={`/ads?state=${adInfo.stateName}&cat=${adInfo.category.slug}`}
+          >
+            {adInfo.category.name}
+          </Link>
+          &gt;
+          {` ${adInfo.title}`}
+        </BreadCrumb>
+      )}
       <PageArea>
         <div className="leftSide">
           <div className="box">
-            <div className="adImage">{loading && <Fake height={300} />}</div>
+            <div className="adImage">
+              {loading && <Fake height={300} />}
+              {adInfo.images && (
+                <Slide>
+                  {adInfo.images.map((img, k) => (
+                    <div key={k} className="each-slide">
+                      <img src={img} alt="" />
+                    </div>
+                  ))}
+                </Slide>
+              )}
+            </div>
             <div className="adInfo">
               <div className="adName">
                 {loading && <Fake height={20} />}
@@ -64,10 +94,8 @@ export default function Page() {
               <div className="adDescription">
                 {loading && <Fake height={100} />}
                 {adInfo.description}
-                <hr/>
-                {adInfo.views && 
-                  <small>Visualizações: {adInfo.views}</small>
-                }
+                <hr />
+                {adInfo.views && <small>Visualizações: {adInfo.views}</small>}
               </div>
             </div>
           </div>
@@ -75,12 +103,45 @@ export default function Page() {
         <div className="rightSide">
           <div className="box box-padding">
             {loading && <Fake height={20} />}
+            {adInfo.priceNegotiable && "Preço Negociável"}
+            {!adInfo.priceNegotiable && adInfo.price && (
+              <div className="price">
+                Preço: <span>R$ {adInfo.price}</span>
+              </div>
+            )}
           </div>
-          <div className="box box-padding">
-            {loading && <Fake height={50} />}
-          </div>
+          {loading && <Fake height={50} />}
+          {adInfo.userInfo && (
+            <>
+              <a
+                href={`mailto:${adInfo.userInfo.email}`}
+                className="sellerLink"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Falar com vendedor(a)
+              </a>
+              <div className="created-by box box-padding">
+                <strong>{adInfo.userInfo.name}</strong>
+                <small>E-mail: {adInfo.userInfo.email}</small>
+                <small>Estado: {adInfo.stateName}</small>
+              </div>
+            </>
+          )}
         </div>
       </PageArea>
+      <OthersArea>
+        {adInfo.others && (
+          <>
+            <h2>Mais ofertas do vendedor</h2>
+            <div className="list">
+              {adInfo.others.map((i, k) => (
+                <AdItem key={k} data={i} />
+              ))}
+            </div>
+          </>
+        )}
+      </OthersArea>
     </PageContainer>
   );
 }
--- frontend/src/pages/AdPage/styled.js
+++ frontend/src/pages/AdPage/styled.js
@@ -24,22 +24,113 @@ export const PageArea = styled.div`
     flex: 1;
     margin-right: 20px;
 
+    .box {
+      display: flex;
+    }
+
     .adImage {
+      width: 320px;
+      height: 320px;
+      margin-right: 20px;
+
+      .each-slide img {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        background-size: cover;
+        height: 320px;
+
+        border-top-left-radius: 5px;
+        border-bottom-left-radius: 5px;
+      }
     }
 
     .adInfo {
-      padding: 10px;
+      flex: 1;
+      margin-right: 15px;
+      margin-bottom: 15px;
 
       .adName {
         margin-bottom: 20px;
+
+        h2 {
+          margin: 0;
+          margin-top: 20px;
+        }
+
+        small {
+          color: #999;
+        }
       }
 
       .adDescription {
+        small {
+          color: #999;
+        }
       }
     }
   }
 
   .rightSide {
     width: 250px;
+
+    .price span {
+      color: blue;
+      display: block;
+      font-size: 27px;
+      font-weight: bold;
+    }
+
+    .sellerLink {
+      background-color: blue;
+      color: white;
+      height: 30px;
+      border-radius: 25px;
+      font-weight: bold;
+      box-shadow: 0px 0px 4px #999;
+
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      text-decoration: none;
+      margin-bottom: 20px;
+    }
+
+    .created-by small {
+      display: block;
+      color: #999;
+      margin-top: 10px;
+    }
+  }
+`;
+
+export const OthersArea = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  h2 {
+    font-size: 20px;
+  }
+
+  .list {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+
+    .adItem {
+      width: 24%;
+    }
+  }
+`;
+
+export const BreadCrumb = styled.div`
+  font-size: 13px;
+  margin-top: 20px;
+
+  a {
+    display: inline-block;
+    margin: 0px 5px;
+    text-decoration: underline;
+    color: black;
   }
 `;
